0b80abe461811ebf9a0694611cd7fe2cfb1e527e
cedric <cedricjoelcayaban15@gmail.com>
2024-04-01 01:46:07 +0800

    updated database

--- database/leave_system.sql
+++ database/leave_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2024 at 05:43 PM
+-- Generation Time: Mar 31, 2024 at 01:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
